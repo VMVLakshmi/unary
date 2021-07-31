f292ecf5971b79242cf79aedb5df9590d7ca5b78
hai <hai@LAPTOP-4DV9N6FJ>
2021-07-31 13:25:45 +0530

    Sixth commit
    Uploading all files

---
+++ Unary/src/pack/Operator.java
@@ -0,0 +1,21 @@
+package pack;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int result = +1;
+System.out.println(result);
+result --;
+System.out.println(result);
+result ++;
+System.out.println(result);
+result=-result;
+System.out.println(result);
+boolean success =false;
+System.out.println(success);
+System.out.println(!success);
+
+	}
+
+}
